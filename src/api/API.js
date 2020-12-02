520dbb83cb8204c1cce6c11db4459cf5fb3430b4
Laurent LELION <l.lelion@callmedicall.com>
2020-12-02 18:53:38 +0100

    added API class

---
+++ src/api/API.js
@@ -0,0 +1,131 @@
+
+// -- axios
+import axios from 'axios';
+
+/**
+ * API
+ * @returns void
+*/
+
+export default class API {
+
+  	constructor(env = null) {
+      console.info(`[${this.constructor.name}]`, env);
+
+      // if (env) {
+      //     this.BASE_URL = env.ENV.API_BASE_URL;
+      //     this.APP_BASE_URL = env.ENV.APP_BASE_URL;
+      // }
+      this.BASE_URL = process.env.REACT_APP_API_BASE_URL;
+    }
+
+    /**
+     * Handle form error messages from Ajax API call
+     * https://getbootstrap.com/docs/4.0/components/forms/
+     * @param {JSON} error
+     * @return string
+     */
+    static handleAPIErrorMessages(error) {
+        //console.info(`[API.handleAPIErrorMessages] error:`, error);
+
+        let errorMessage = {
+            title: "",
+            body: ""
+        };
+
+        if (typeof(error) !== "undefined"){
+
+            // Status
+            if (typeof(error.status) !== "undefined"){
+                errorMessage.title += `Error ${error.status}`;
+            }
+            if (typeof(error.statusText) !== "undefined"){
+                errorMessage.title += ` : ${error.statusText}`;
+            }
+
+            // Body
+            if (typeof(error.data) !== "undefined"){
+
+                if (typeof(error.data.errors) !== "undefined") {
+
+                    for (let key in error.data.errors) {
+                        errorMessage.body += `${error.data.errors[key]}\n`;
+                    }
+                }
+                else if (typeof(error.data.message) !== "undefined" ){
+                    errorMessage.body += `${error.data.message}`;
+                }
+            }
+        }
+
+
+        // 403 ..
+        //let errorMessage = error.responseJSON.message; // global error message
+
+        return errorMessage.title !== "" ? errorMessage : {
+            title: "Internal server error",
+            body: "Internal server error"
+        };
+    }
+
+
+    //-------------------------------------------------------------------------
+    //                            Books
+    //-------------------------------------------------------------------------
+
+    /**
+    * Get all books
+      ex : http://henri-potier.xebia.fr/books
+    * @returns Promise
+    */
+    getBooks = async () => {
+      console.info(`[${this.constructor.name}.getBooks]`);
+
+      const APIRoute = `${this.BASE_URL}/books`;
+
+      const response = await axios.get(APIRoute)
+      .then(json => {
+          //console.log(`[${this.constructor.name}.getBooks] json`, json);
+          const books = json.data.data;
+          return books;
+      })
+      // .catch(error => {
+      //     // TODO : 404 ...
+      //     console.error(`[${this.constructor.name}.getElearning]`, error.response);
+      // })
+      //.finally(() => this.setState({ loading: false }))
+
+      return response;
+    }
+
+    //-------------------------------------------------------------------------
+    //                            CommercialOffers
+    //-------------------------------------------------------------------------
+
+    /**
+    * Get commercialOffers for a book
+      ex : http://henri-potier.xebia.fr/books/c8fabf68-8374-48fe-a7ea-a00ccd07afff/commercialOffers
+    * @param {string} isbn
+    * @returns Promise
+    */
+    getBookCommercialOffers = async (isbn) => {
+      console.info(`[${this.constructor.name}.login]`, isbn);
+
+      const APIRoute = `${this.BASE_URL}/books/${isbn}/commercialOffers`;
+
+      const response = await axios.get(APIRoute)
+      .then(json => {
+          //console.log(`[${this.constructor.name}.getBooks] json`, json);
+          const commercialOffers = json.data.data;
+          return commercialOffers;
+      })
+      // .catch(error => {
+      //     // TODO : 404 ...
+      //     console.error(`[${this.constructor.name}.getElearning]`, error.response);
+      // })
+      //.finally(() => this.setState({ loading: false }))
+
+      return response;
+    }
+
+}
